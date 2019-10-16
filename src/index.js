2fc410d9ff850f0d19b8114788ffb6fbab0263ac
zhangrh <zhangrhweb@163.com>
2019-10-16 14:54:07 +0800

    完成多个模型放到同一个中

--- src/index.js
+++ src/index.js
@@ -21,45 +21,46 @@ function init() {
 
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.2, 2000);
   camera.position.set(10, 10, 80);
-  camera.lookAt(0, 0, 0);
+  // camera.lookAt(0, 0, 20);
 
   scene = new THREE.Scene();
 
-  var loader = new ColladaLoader(); 
+  var loader = new ColladaLoader();
   loader.load('../dae/Dragon 2.5_dae.dae', function (collada) {
     var dargon = collada.scene
-    scene.add(dargon)
+    dargon.scale.copy({
+      x: 0.5,
+      y: 0.5,
+      z: 0.5
+    })
+    scene.add(collada.scene);
   });
+
   loader.load('../modules/collada/elf/elf.dae', function (collada) {
     var girl = collada.scene
-    girl.position.copy({
-      x: 10,
-      y: 0,
-      z: 10
-    })
-    scene.add(girl)
+    girl.position.x = 10
+    scene.add(girl);
   });
-  loader.load( '../modules/collada/stormtrooper/stormtrooper.dae', function ( collada ) {
+
+  loader.load('../modules/collada/stormtrooper/stormtrooper.dae', function (collada) {
     var animations = collada.animations;
     var avatar = collada.scene;
-    avatar.position.z = 24
-    avatar.traverse( function ( node ) {
-      if ( node.isSkinnedMesh ) {
-        node.frustumCulled = false;
-      }
-    } );
-    mixer = new THREE.AnimationMixer( avatar );
-    mixer.clipAction( animations[0]).play();
+    avatar.position.z = 20
+    mixer = new THREE.AnimationMixer(avatar);
+    mixer.clipAction(animations[0]).play();
     scene.add(avatar);
-  } );
+  });
+
 
   renderer = new THREE.WebGLRenderer();
+  var gridHelper = new THREE.GridHelper(100, 10);
+  scene.add(gridHelper)
 
   container.appendChild(renderer.domElement);
   control = new OrbitControls(camera, renderer.domElement.parentNode);
-  control.target=new THREE.Vector3(0,0,0);
+  control.target = new THREE.Vector3(0, 0, 0);
   stats = new Stats();
-  container.appendChild( stats.dom );
+  container.appendChild(stats.dom);
   window.addEventListener('resize', onWindowResize, false);
 }
 
@@ -77,7 +78,7 @@ function animate() {
 
 function render() {
   var delta = clock.getDelta();
-  if ( mixer !== undefined ) {
+  if (mixer !== undefined) {
     mixer.update(delta); // 根据当前的一个时间更新动画
   }
   renderer.render(scene, camera);
