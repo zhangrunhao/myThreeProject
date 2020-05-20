fee1d37a9590dce926956dbd4ae169f4ab8dedb4
zhangrh <zhangrhweb@163.com>
2020-05-20 11:18:35 +0800

    demo02

---
+++ src/demo02/index.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three'
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls'
+import vrJpgArr from './vr/vr'
+
+var camera, scene, renderer, controls;
+
+function init() {
+  var container = document.getElementById('container');
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement)
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100)
+  camera.position.z = 0.01;
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+
+  var materials = getMaterials();
+  var skyBox = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), materials);
+  skyBox.geometry.scale(1, 1, -1)
+  scene.add(skyBox)
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+function getMaterials() {
+  var textures = []
+  var materials = []
+  for (var i = 0; i < vrJpgArr.length; i++) {
+    textures[i] = new THREE.Texture();
+    var img  = new Image()
+    img.src = vrJpgArr[i];
+    textures[i].image = img;
+    textures[i].needsUpdate = true;
+    materials.push(new THREE.MeshBasicMaterial({
+      map: textures[i]
+    }))
+  }
+  return materials;
+}
+
+init();
+animate();
---
+++ src/demo02/vr/vr.js
@@ -0,0 +1,10 @@
+import j1 from './1.jpg'
+import j2 from './2.jpg'
+import j3 from './3.jpg'
+import j4 from './4.jpg'
+import j5 from './5.jpg'
+import j6 from './6.jpg'
+
+export default [
+  j1, j2, j3, j4, j5, j6
+]
